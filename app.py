6293d6440a312289708e5e8b46294ab93fe11b53
Jugal kishor <127476866+Jugal2023@users.noreply.github.com>
2023-04-25 21:13:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 app=Flask(__name__)
 ## load the model
 regmodel=pickle.load(open('regmodel.pkl','rb'))
-scalar=pickle.load(open('scaling.pkl','rb'))
+scaler=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -18,7 +18,7 @@ def predict_api():
     data=request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
